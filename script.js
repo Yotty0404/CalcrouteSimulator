ad887b3522d044f137f58f0b7cd761c8877b7c4f
よってぃ <74546201+Yotty0404@users.noreply.github.com>
2022-11-24 14:46:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var isDispKeyboard = false;
-var moveHistory = []
+var moveHistory = [];
 var lastMoveDirection = "";
 var sameMoveCount = 0;
 var topNum = 0;
@@ -19,7 +19,6 @@ $(window).on('load', function (){
     cell.appendTo("#stage");
   }
 });
-
 
 var timer = false;
 $(window).resize(function(){
@@ -79,9 +78,20 @@ function Move(direction){
 
   Calc(direction);
 
+
+  if(lastMoveDirection != direction){
+    moveHistory.push({direction:direction, sameMoveCount:1});
+  }
+  else{
+    moveHistory.slice(-1)[0].sameMoveCount += 1;
+  }
+
   $("#piece").attr("value", val);
   var offset = $(`.cell[value=${val}]`).offset();
   $("#piece").offset({ top: offset.top, left: offset.left });
+
+  lastMoveDirection = direction;
+  DispMove();
 }
 
 function Calc(direction){
@@ -94,14 +104,14 @@ function Calc(direction){
 
   switch (direction) {
     case 'u':
-      if(lastMoveDirection == direction)bottomNum*=sameMoveCount-1
+      if(lastMoveDirection == direction)bottomNum*=sameMoveCount-1;
       topNum*=sameMoveCount;
       break;
     case 'r':
       topNum+=bottomNum;
       break;
     case 'd':
-      if(lastMoveDirection == direction)topNum*=sameMoveCount-1
+      if(lastMoveDirection == direction)topNum*=sameMoveCount-1;
       bottomNum*=sameMoveCount;
       break;
     case 'l':
@@ -109,23 +119,103 @@ function Calc(direction){
       break;
   }
 
+  DispCalcResult();
+}
+
+function Back(){
+  if(!$('#piece').length) return;
+  if(isDispKeyboard)return;
+  if(moveHistory.length==0)return;
+
+  var direction = moveHistory.slice(-1)[0].direction;
+
+  var val = Number($("#piece").attr("value"));
+  switch (direction) {
+    case 'u':
+      val+=5;
+      break;
+    case 'r':
+      val-=1;
+      break;
+    case 'd':
+      val-=5;
+      break;
+    case 'l':
+      val+=1;
+      break;
+  }
+
+  CalcForBack(direction);
+
+  if(moveHistory.slice(-1)[0].sameMoveCount==1){
+    moveHistory.pop();
+
+    if(moveHistory.length==0){
+      lastMoveDirection = "";
+      sameMoveCount=0;
+    }
+    else{
+      lastMoveDirection = moveHistory.slice(-1)[0].direction;
+      sameMoveCount=moveHistory.slice(-1)[0].sameMoveCount;
+    }
+  }
+  else{
+    moveHistory.slice(-1)[0].sameMoveCount-=1;
+    sameMoveCount = moveHistory.slice(-1)[0].sameMoveCount;
+  }
+
+  $("#piece").attr("value", val);
+  var offset = $(`.cell[value=${val}]`).offset();
+  $("#piece").offset({ top: offset.top, left: offset.left });
+  DispMove();
+}
+
+function CalcForBack(direction){
+  sameMoveCount = moveHistory.slice(-1)[0].sameMoveCount;
+
+  switch (direction) {
+    case 'u':
+      bottomNum*=sameMoveCount;
+      if(sameMoveCount!=1)topNum*=sameMoveCount-1;
+      break;
+    case 'r':
+      topNum-=bottomNum;
+      break;
+    case 'd':
+      topNum*=sameMoveCount;
+      if(sameMoveCount!=1)bottomNum*=sameMoveCount-1;
+      break;
+    case 'l':
+      topNum+=bottomNum;
+      break;
+  }
+
+  DispCalcResult();
+}
+
+function DispCalcResult(){
   var gcdNum = gcd(topNum, bottomNum);
   topNum/= gcdNum;
   bottomNum/= gcdNum;
+  //マイナスを分子に表示
+  if(bottomNum < 0){
+    topNum*=-1;
+    bottomNum*=-1;
+  }
 
   $('#piece').removeClass("piece_fraction");
   $('#piece').text(topNum);
-  if(bottomNum!=1)CraeteFraction(topNum, bottomNum);
-  lastMoveDirection = direction;
+  if(bottomNum!=1)CreateFraction(topNum, bottomNum);
 }
 
-function CraeteFraction(topNum, bottomNum){
+function CreateFraction(topNum, bottomNum){
   $('#piece').addClass("piece_fraction");
   $('#piece').empty();
   $("#piece").append(`<span class="numerator">${topNum}</span>`)
   $("#piece").append(`<span class="divide">／</span>`)
   $("#piece").append(`<span class="denominator">${bottomNum}</span>`)
 }
+
 
 function CanMove(direction){
   var val = Number($("#piece").attr("value"));
@@ -147,8 +237,13 @@ function CanMove(direction){
 
 $(document).on("click", ".num", function () {
   if(!$('#piece').length) return;
-  if($('#piece').text() == "" && $(this).text()=="0") return;
-  var text = $('#piece').text() + $(this).text()
+  if($('#piece').text()=="0"){
+    var text = $(this).text()
+    }
+  else{
+    var text = $('#piece').text() + $(this).text()
+    }
+
   $('#piece').text(text);
 });
 
@@ -160,12 +255,7 @@ $(document).on("click", "#key_back", function () {
 $(document).on("click", "#key_enter", function () {
   if($('#piece').text()=="") return;
   switchDispKeyboard();
-
-  //初期化
-  topNum = Number($('#piece').text());
-  bottomNum = 1;
-  sameMoveCount=0;
-  lastMoveDirection = "";
+  Reset();
 });
 
 function switchDispKeyboard(){
@@ -244,4 +334,72 @@ function onTouchEnd(event) {
 
 $(document).on("click", "#btn_delete", function () {
   $("#piece").remove();
+  $('#row_moves').empty();
+  Reset();
 });
+
+
+$(document).on("click", "#btn_back", function () {
+  Back();
+});
+
+
+$(document).on("click", "#btn_restart", function () {
+  while(moveHistory.length>0){
+    Back();
+  }
+  Reset();
+});
+
+
+function DispMove(){
+  $("#moves_dummy").hide();
+  $("#move_excess").hide();
+  var roopCount = Math.min(moveHistory.length, 8);
+  $('#row_moves').empty();
+  for (let i = 0; i < roopCount; i++) {
+    var symbol = "";
+    var str_class = "";
+    switch (moveHistory[i].direction) {
+      case 'u':
+        symbol = "×";
+        str_class = "move_times";
+        break;
+      case 'r':
+        symbol = "+";
+        str_class = "move_plus";
+        break;
+      case 'd':
+        symbol = "÷";
+        str_class = "move_divided";
+        break;
+      case 'l':
+        symbol = "－";
+        str_class = "move_minus";
+        break;
+    }
+    $("#row_moves").append(`<div class="${str_class}">${symbol+moveHistory[i].sameMoveCount}</div>`)
+  }
+
+  if(moveHistory.length > 8){
+    $("#moves_dummy").show();
+    $("#move_excess").show();
+    
+    $("#move_excess").addClass('moves_animation');
+    setTimeout(() => {$("#move_excess").removeClass('moves_animation')}, 200);
+    ;
+  }
+  else{
+    $($("#row_moves").children().slice(-1)[0]).addClass('moves_animation');
+  }
+}
+
+function Reset(){
+  //初期化
+  topNum = Number($('#piece').text());
+  bottomNum = 1;
+  sameMoveCount=0;
+  lastMoveDirection = "";
+  moveHistory = [];
+  DispMove();
+}
